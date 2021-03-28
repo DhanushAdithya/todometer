77985582f23ce70973d3b26eac8c0a7072480dd2
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-03-28 22:37:57 +0530

    Dummy component setup (for now)

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Navbar } from './components'
+
+function App() {
+	return (
+		<div>
+			<Navbar />
+		</div>
+	)
+}
+
+export default App
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import '../styles/Navbar.scss'
+
+const Navbar = () => {
+	return (
+		<div>
+			<nav class='flex justify-center py-2'>
+				<h1>Todometer</h1>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
