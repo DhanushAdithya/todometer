57bc80a040e2da5a17c39880c8ddbe9a37374f8d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-03-30 22:22:21 +0530

    Removed return statement

--- frontend/src/components/TodoAdd.js
+++ frontend/src/components/TodoAdd.js
@@ -3,14 +3,12 @@ import { TiPlus } from 'react-icons/ti'
 import '../styles/Card.scss'
 import '../styles/TodoAdd.scss'
 
-const TodoAdd = () => {
-	return (
-		<div className='card flex justify-center items-center'>
-			<div className='icon-container'>
-				<TiPlus className='icon' />
-			</div>
+const TodoAdd = () => (
+	<div className='card flex justify-center items-center'>
+		<div className='icon-container'>
+			<TiPlus className='icon' />
 		</div>
-	)
-}
+	</div>
+)
 
 export default TodoAdd
