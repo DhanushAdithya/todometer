48dc1bc69ada89d49211818ec1e3832f4c4a82d0
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-03-30 22:24:21 +0530

    Added useLocalStorage hook

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -1,11 +1,14 @@
 import Card from './Card'
 import TodoAdd from './TodoAdd'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 const Container = () => {
+	const [todo] = useLocalStorage('todometer-ds-crusaders')
+
 	return (
 		<div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 p-5 place-items-center'>
-			{['Morning', 'Afternoon', 'Night'].map(e => (
-				<Card name={e} />
+			{todo.todos.map(({ name, done, later, undone }) => (
+				<Card name={name} done={done} later={later} undone={undone} />
 			))}
 			<TodoAdd />
 		</div>
