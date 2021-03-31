846e488c494ecee8dacfa5fb2c090bf0580fafae
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-03-31 21:34:02 +0530

    Applied store with index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './store'
 import './styles/index.css'
 import App from './App'
 
-render(<App />, document.getElementById('root'))
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
