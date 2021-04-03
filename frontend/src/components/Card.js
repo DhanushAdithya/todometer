e99eed4e180586e1575f97d1730181bd93dcd50e
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-04-03 20:35:56 +0530

    Added action creators

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,11 +1,17 @@
-import { FaClock, FaTimes, FaCheck, FaPlus } from 'react-icons/fa'
+import { FaPause, FaTimes, FaCheck, FaPlus } from 'react-icons/fa'
+import { connect } from 'react-redux'
+import { Link } from 'wouter'
+import { removeGroup } from '../store/actions/actionCreators'
 
 import '../styles/Card.scss'
 
-const Card = ({ name, done, later, undone }) => (
+const Card = ({ name, done, later, undone, id, deleteGroup }) => (
 	<div className='card grid grid-rows-3 py-2 pt-0'>
 		<div className='pl-4 flex items-center mb-2'>
-			<div className='w-4 h-4 bg-red-400 rounded-full cursor-pointer hover:bg-red-500 transition'></div>
+			<div
+				className='w-4 h-4 bg-red-400 rounded-full cursor-pointer hover:bg-red-500 transition'
+				onClick={() => deleteGroup(name)}
+			></div>
 			<h1 className='text-2xl font-bold text-gray-300 ml-3'>{name}</h1>
 		</div>
 		<div className='grid grid-cols-3 p-3 pt-0 gap-x-3'>
@@ -14,7 +20,7 @@ const Card = ({ name, done, later, undone }) => (
 				<div className='text-gray-300 font-bold text-xl'>{done}</div>
 			</div>
 			<div className='flex rounded-full justify-around items-center p-1 bg-gray-500 shadow-sm hover:shadow-lg'>
-				<FaClock className='text-2xl text-yellow-400' />
+				<FaPause className='text-xl text-yellow-400' />
 				<div className='text-gray-300 font-bold text-xl'>{later}</div>
 			</div>
 			<div className='flex rounded-full justify-around items-center p-1 bg-gray-500 shadow-sm hover:shadow-lg'>
@@ -23,11 +29,17 @@ const Card = ({ name, done, later, undone }) => (
 			</div>
 		</div>
 		<div className='grid grid-cols-1 h-16 pb-3 place-items-center'>
-			<div className='grid place-items-center h-12 w-12 rounded-full cursor-pointer border-blue-300 border-2 text-blue-300 transition hover:bg-blue-300 hover:text-gray-600'>
-				<FaPlus className='text-xl' />
-			</div>
+			<Link href={`/todo/${id}`}>
+				<div className='grid place-items-center h-12 w-12 rounded-full cursor-pointer border-blue-300 border-2 text-blue-300 transition hover:bg-blue-300 hover:text-gray-600'>
+					<FaPlus className='text-xl' />
+				</div>
+			</Link>
 		</div>
 	</div>
 )
 
-export default Card
+const mapDispatchToProps = dispatch => ({
+	deleteGroup: name => dispatch(removeGroup(name)),
+})
+
+export default connect(null, mapDispatchToProps)(Card)
