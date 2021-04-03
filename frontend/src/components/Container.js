0ff9bc748a22a4ead22837db426848d8365dcbfb
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-04-03 20:33:12 +0530

    Updated Container with redux store

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -1,18 +1,25 @@
+import { connect } from 'react-redux'
+
 import Card from './Card'
 import TodoAdd from './TodoAdd'
-import useLocalStorage from '../hooks/useLocalStorage'
 
-const Container = () => {
-	const [todo] = useLocalStorage('todometer-ds-crusaders')
-
-	return (
-		<div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 p-5 place-items-center'>
-			{todo.todos.map(({ name, done, later, undone }) => (
-				<Card name={name} done={done} later={later} undone={undone} />
+const Container = ({ todos }) => (
+	<div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 p-5 place-items-center'>
+		{todos &&
+			todos.map(({ group, items }) => (
+				<Card
+					key={group.toLowerCase().split(/\s+/).join('-')}
+					id={group.toLowerCase().split(/\s+/).join('-')}
+					name={group}
+					done={items.filter(e => e.state === 'done').length}
+					later={items.filter(e => e.state === 'later').length}
+					undone={items.filter(e => e.state === 'undone').length}
+				/>
 			))}
-			<TodoAdd />
-		</div>
-	)
-}
+		<TodoAdd />
+	</div>
+)
 
-export default Container
+const mapStateToProps = todos => todos
+
+export default connect(mapStateToProps, null)(Container)
